08283e6531ac3436a6cd3e56eca34e8f6b25dec6
Ben Manley <benjamintmanley@gmail.com>
2022-08-31 19:46:29 -0500

    add color squares

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('sanity check');
+let colors = ['cornflowerblue', 'red', 'tan', 'fuchsia', 'blanchedalmond', 'mediumspringgreen', 'rebeccapurple','thistle','papayawhip', 'lime']
+
+let colorSquareContainer = document.querySelector('.container')
+
+console.log(colorSquareContainer);
+
+// loop through the colors array
+colors.forEach(color => {
+  // create a div
+  let newDiv = document.createElement('div')
+  newDiv.style.backgroundColor = color
+  // assign the style property of the div to color the background
+  newDiv.className = 'color-square'
+  // append div to a container element
+  colorSquareContainer.appendChild(newDiv)
+})
