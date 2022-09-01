5e931e1977af176adec4a8b91246afe99a96fb2a
Ben Manley <benjamintmanley@gmail.com>
2022-08-31 20:58:03 -0500

    add hover functions

--- js/app.js
+++ js/app.js
@@ -5,18 +5,20 @@ let colors = ['cornflowerblue', 'red', 'tan', 'fuchsia', 'blanchedalmond', 'medi
 
 /*----------------- Variables (state) -------------*/
 let selectedColor
-
+let lastClicked
 
 
 /*--------- Cached Element References ------------*/
 let colorSquareContainer = document.getElementById('square-container')
 let selectedColorSquare = document.getElementById('selected-color')
-
+let resetBtn = document.querySelector('button')
 
 
 /*---------- Event Listeners -----------------*/
 colorSquareContainer.addEventListener('click', handleClickColor)
-
+colorSquareContainer.addEventListener('mouseover', handleMouseOver)
+colorSquareContainer.addEventListener('mouseout', handleMouseOut)
+resetBtn.addEventListener('click', init)
 
 /*----------------- Functions -------------------*/
 
@@ -27,7 +29,19 @@ function handleClickColor(evt) {
   // determine which square was clicked
   // get the color from that square 
   // set selectedColor to that color
+  console.log('handle click running')
   selectedColor = evt.target.style.backgroundColor
+  lastClicked = evt.target.style.backgroundColor
+  render()
+}
+
+function handleMouseOver(evt) {
+  selectedColor = evt.target.style.backgroundColor
+  render()
+}
+
+function handleMouseOut(evt) {
+  selectedColor = lastClicked
   render()
 }
 
@@ -46,10 +60,12 @@ colors.forEach(color => {
 function init() {
   // setting state variables to initial state
   selectedColor = 'cornflowerblue'
+  lastClicked = 'cornflowerblue'
   render()
 }
 
 function render() {
   // display updated state
   selectedColorSquare.style.backgroundColor = selectedColor
+  selectedColorSquare.innerText = selectedColor
 }
